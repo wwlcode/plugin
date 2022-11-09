df9540c4955b4804605b167ec18ce9a8887e35de
GitHub Action <action@github.com>
2022-11-09 06:11:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/wwlcode/plugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wwlcode/plugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wwlcode.plugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wwlcode.plugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/wwlcode/plugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wwlcode/plugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.wwlcode.plugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wwlcode.plugin.MyBundle
 
 class MyProjectService(project: Project) {
 
